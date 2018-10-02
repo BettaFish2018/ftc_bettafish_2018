091d4e290c52e6a0ecc874604c9abc083b466333
Tej Bade <badetej5@gmail.com>
2018-10-02 07:32:31 -0700

    Tej Test

--- ftc_app-4.0/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java
+++ ftc_app-4.0/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java
@@ -44,7 +44,7 @@ import com.qualcomm.robotcore.util.Range;
  *
  * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
  * It includes all the skeletal structure that all iterative OpModes contain.
- *
+ * Test
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
